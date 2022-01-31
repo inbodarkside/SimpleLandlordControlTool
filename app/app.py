3117eebcd7d2baaf47724cc5fdefa9c145989918
ggarcia <ggarcia@purpleblob.net>
2022-01-31 13:54:31 +0100

    FEAT: Added error 500 and finished common style

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ import requests
 from datetime import datetime, timedelta
 from flask import Flask, render_template, request
 import pandas as pd
+from werkzeug.debug import get_current_traceback
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 
 @app.errorhandler(404)
@@ -13,15 +14,21 @@ def not_found_error(error):
 
 @app.errorhandler(500)
 def internal_error(error):
+    #return "500 error"
     return render_template('500.html')
 
 
 @app.errorhandler(Exception)
 def handle_error(e):
+    #return "500 error"
     return render_template('500.html')
 
 
 @app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
@@ -39,4 +46,4 @@ def send():
 
 
 if __name__ == '__main__':
-    app.run(debug =True)
+    app.run(debug=True)
